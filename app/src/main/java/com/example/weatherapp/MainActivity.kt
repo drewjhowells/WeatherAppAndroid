6d97d230a0890070c8d94251bfac9273c7e22745
dmalasky <dmalasky@byui.edu>
2024-03-13 18:04:26 -0600

    Changed content

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -16,15 +16,7 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			WeatherAppTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colorScheme.background
-				) {
-					Greeting("Android")
-				}
-			}
+			Text(text = "Weather = COLD")
 		}
 	}
 }
