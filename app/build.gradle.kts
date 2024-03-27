9702b721643fce4e6f37d95c70c6963b1a8fac01
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2024-03-27 11:25:01 -0600

    Packages and setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
+	id("com.google.gms.google-services")
 }
 
 android {
@@ -59,6 +60,9 @@ dependencies {
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
+	implementation ("androidx.appcompat:appcompat:1.3.1")
+	implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+	implementation("com.google.firebase:firebase-analytics")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
